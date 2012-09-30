b66acd6c650b5aeeaf181a1a22d6a62883818654
Kovalev Filipp <kovalev@quantion.ru>
2012-09-30 20:54:43 +0400

    Fixed random

--- server/main.cpp
+++ server/main.cpp
@@ -3,9 +3,12 @@
 #include "pserver.h"
 #include "pthreadpool.h"
 #include "psetting.h"
+#include <cstdlib>
+#include <ctime>
 
 int main(int argc, char** argv)
 {
+    qsrand(time(NULL));
     QCoreApplication app(argc, argv);
 
     pSetting setting("../serverSettings.ini");
