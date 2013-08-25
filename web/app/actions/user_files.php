cbe69eb494246b81aa6d00b6b77f8e7ae47c3f63
Filipp Ozinov <ozinov@quantion.ru>
2013-08-25 12:10:33 +0400

    More detailed exception

--- web/app/actions/user_files.php
+++ web/app/actions/user_files.php
@@ -43,7 +43,7 @@
 				$user->load();
 			}
 			catch(ApplicationModelException_User $e) {
-                throw new ApplicationException('Cannot load user.', 500);
+                throw new ApplicationException($e->getMessage(), 500);
 			}
 			
 			// get the page number
