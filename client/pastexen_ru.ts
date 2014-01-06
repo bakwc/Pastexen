37d71101092cdb888a14c7a8837776a97b869d79
Anton Konstantinov <falceeffected@gmail.com>
2014-01-06 19:23:17 +0400

    Updated translation

--- client/pastexen_ru.ts
+++ client/pastexen_ru.ts
@@ -1,26 +1,25 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
-<defaultcodec>UTF-8</defaultcodec>
+<TS version="2.1" language="ru_RU">
 <context>
     <name>Application</name>
     <message>
-        <location filename="application.cpp" line="258"/>
+        <location filename="application.cpp" line="286"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="262"/>
+        <location filename="application.cpp" line="290"/>
         <source>Configure</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="263"/>
+        <location filename="application.cpp" line="291"/>
         <source>My files</source>
         <translation>Мои файлы</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="265"/>
+        <location filename="application.cpp" line="293"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
@@ -29,7 +28,7 @@
         <translation type="obsolete">Ошибка!</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="535"/>
+        <location filename="application.cpp" line="569"/>
         <source>No text found in clipboard</source>
         <translation>В буфере обмена нет текста</translation>
     </message>
@@ -46,63 +45,63 @@
         <translation type="obsolete">Утилита для шаринга скриншотов и кода&lt;br&gt; Наш сайт &lt;a href=&quot;http://pastexen.com/&quot;&gt;pastexen.com&lt;/a&gt;&lt;br&gt; Хостинг предоставлен &lt;a href=&quot;http://scalaxy.ru/&quot;&gt;scalaxy.ru&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="161"/>
-        <location filename="application.cpp" line="179"/>
-        <location filename="application.cpp" line="349"/>
-        <location filename="application.cpp" line="360"/>
+        <location filename="application.cpp" line="164"/>
+        <location filename="application.cpp" line="184"/>
+        <location filename="application.cpp" line="377"/>
+        <location filename="application.cpp" line="389"/>
         <source>Uploading image...</source>
         <translation>Отправка изображения...</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="167"/>
+        <location filename="application.cpp" line="170"/>
         <source>Failed to load image</source>
         <translation>Не удалось загрузить изображение</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="194"/>
-        <location filename="application.cpp" line="204"/>
-        <location filename="application.cpp" line="382"/>
-        <location filename="application.cpp" line="541"/>
+        <location filename="application.cpp" line="199"/>
+        <location filename="application.cpp" line="211"/>
+        <location filename="application.cpp" line="412"/>
+        <location filename="application.cpp" line="575"/>
         <source>Uploading code...</source>
         <translation>Отправка кода...</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="197"/>
+        <location filename="application.cpp" line="202"/>
         <source>Failed to open file</source>
         <translation>Не удалось открыть файл</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="208"/>
+        <location filename="application.cpp" line="215"/>
         <source>Unknown file type</source>
         <translation>Неизвестный тип файла</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="451"/>
+        <location filename="application.cpp" line="481"/>
         <source>Utility for easy screenshoting and code sharing&lt;br&gt; We live at &lt;a href=&quot;http://pastexen.com/&quot;&gt;pastexen.com&lt;/a&gt;</source>
         <translation>Утилита для шаринга скриншотов и кода&lt;br&gt;Наш сайт &lt;a href=&quot;http://pastexen.com/&quot;&gt;pastexen.com&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="461"/>
+        <location filename="application.cpp" line="491"/>
         <source>Text share (%1)</source>
         <translation>Текст (%1)</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="462"/>
+        <location filename="application.cpp" line="492"/>
         <source>Full s-shot (%1)</source>
         <translation>Полн. экран (%1)</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="463"/>
+        <location filename="application.cpp" line="493"/>
         <source>Half s-shot (%1)</source>
         <translation>Часть экр. (%1)</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="468"/>
+        <location filename="application.cpp" line="498"/>
         <source>Auto detection</source>
         <translation>Авто-определение</translation>
     </message>
     <message>
-        <location filename="application.cpp" line="469"/>
+        <location filename="application.cpp" line="499"/>
         <source>Plain text</source>
         <translation>Обычный текст</translation>
     </message>
@@ -110,57 +109,67 @@
 <context>
     <name>ConfigForm</name>
     <message>
-        <location filename="config.ui" line="48"/>
+        <location filename="config.ui" line="304"/>
         <source>Apply</source>
         <translation>Применить</translation>
     </message>
     <message>
-        <location filename="config.ui" line="55"/>
+        <location filename="config.ui" line="311"/>
         <source>Cancel</source>
         <translation>Отмена</translation>
     </message>
     <message>
-        <location filename="config.ui" line="71"/>
+        <location filename="config.ui" line="163"/>
         <source>Code share:</source>
         <translation>Шаринг кода:</translation>
     </message>
     <message>
-        <location filename="config.ui" line="104"/>
+        <location filename="config.ui" line="184"/>
         <source>Code share</source>
         <translation>Шаринг кода</translation>
     </message>
     <message>
-        <location filename="config.ui" line="145"/>
+        <location filename="config.ui" line="225"/>
         <source>Syntax:</source>
         <translation>Синтаксис:</translation>
     </message>
     <message>
-        <location filename="config.ui" line="186"/>
+        <location filename="config.ui" line="257"/>
         <source>Display syntax selection dialog</source>
         <translation>Отображать диалог выбора языка</translation>
     </message>
     <message>
-        <location filename="config.ui" line="221"/>
+        <location filename="config.ui" line="270"/>
+        <source>General</source>
+        <translation>Основные настройки</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="276"/>
+        <source>Start on system boot</source>
+        <translation>Запускаться при загрузке системы</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="153"/>
         <source>Edit screenshots before sharing</source>
         <translation>Редактировать скриншоты</translation>
     </message>
     <message>
-        <location filename="config.ui" line="235"/>
+        <location filename="config.ui" line="36"/>
         <source>Image share</source>
         <translation>Шаринг скриншотов</translation>
     </message>
     <message>
-        <location filename="config.ui" line="268"/>
+        <location filename="config.ui" line="60"/>
         <source>Partial screenshot:</source>
         <translation>Часть экрана:</translation>
     </message>
     <message>
-        <location filename="config.ui" line="303"/>
+        <location filename="config.ui" line="130"/>
         <source>Image type:</source>
         <translation>Тип скриншота:</translation>
     </message>
     <message>
-        <location filename="config.ui" line="341"/>
+        <location filename="config.ui" line="95"/>
         <source>Full screenshot:</source>
         <translation>Полный экран:</translation>
     </message>
@@ -173,7 +182,7 @@
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="configwidget.cpp" line="74"/>
+        <location filename="configwidget.cpp" line="75"/>
         <source>&lt;a href=&quot;%1&quot;&gt;My account&lt;/a&gt; (you need to share file first)</source>
         <translation>&lt;a href=&quot;%1&quot;&gt;Мой аккаунт&lt;/a&gt; (вначале что-то отправьте)</translation>
     </message>
